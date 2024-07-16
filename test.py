e6a0b7c2163b3da3ebcd0367eca37c980da72378
Zhai <mzhai72@d240614.stjude.sjcrh.local>
2024-07-16 09:52:48 -0500

    aasdfsdfa

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-print("hello world");
+import flowkit as fk
+
+fcs_path = "/Users/mzhai72/Desktop/Flow Cytometry Data/6-28-24 Mouse Takedown Datas/A2 106 High_Unmixed.fcs"
+sample = fk.Sample(fcs_path)
